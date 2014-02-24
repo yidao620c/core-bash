473901d35a135ac86d4db9c205ade11373853e06
XiongNeng <yidao620@gmail.com>
2014-02-24 14:06:32 +0800

    update something...

--- 04_autotest/auto_test.sh
+++ 04_autotest/auto_test.sh
@@ -4,14 +4,15 @@ export DISPLAY=:0
 HOME="/home/mango"
 
 echo "starting auto run test..."
+rm -f $HOME/work/rubyselenium/config/config.yml
 cd $HOME/work/rubyselenium/
 svn update
 wait
 echo "update finished mango..."
-
+cp -f $HOME/work/rubyselenium/config/config_cool.yml $HOME/work/rubyselenium/config/config.yml
 rspec testcase -fh > report.html
 wait
 echo "test finished ..."
 
-tod=$(date +%Y%m%d_%H%M%S)
-cp $HOME/work/rubyselenium/report.html $HOME/work/reports/report_$tod.html
+tod=$(date +%Y%m%d)
+cp -f $HOME/work/rubyselenium/report.html $HOME/work/reports/report_$tod.html
