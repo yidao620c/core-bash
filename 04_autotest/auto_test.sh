ee5c955d0f39a91e577169f30fe647a9237b2fb6
XiongNeng <yidao620@gmail.com>
2014-02-25 16:46:51 +0800

    auto mail

--- 04_autotest/auto_test.sh
+++ 04_autotest/auto_test.sh
@@ -2,6 +2,8 @@
 export DISPLAY=:0
 
 HOME="/home/mango"
+SERVER_HOME="/home/orchard"
+REMOTE_ADDR="orchard@10.0.0.191"
 
 echo "starting auto run test..."
 rm -f $HOME/work/rubyselenium/config/config.yml
@@ -16,3 +18,8 @@ echo "test finished ..."
 
 tod=$(date +%Y%m%d)
 cp -f $HOME/work/rubyselenium/report.html $HOME/work/reports/report_$tod.html
+ssh -T orchard@10.0.0.191 rm -f $REMOTE_ADDR:$SERVER_HOME/work/report.html
+scp $HOME/work/rubyselenium/report.html $REMOTE_ADDR:$SERVER_HOME/work/
+wait
+echo "report transfer finished"
+ssh -T orchard@10.0.0.191 $SERVER_HOME/work/mail.sh
