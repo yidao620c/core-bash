3ea5f67ab330903080798074de443af2419024be
XiongNeng <yidao620@gmail.com>
2014-05-05 14:43:43 +0800

    update auto publish shell

--- 04_autotest/auto_publish.sh
+++ 04_autotest/auto_publish.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# 这个mango系统的自动发布脚本
 
 SERVER_HOME="/home/orchard"
 REMOTE_ADDR="orchard@10.0.0.173"
+MANGO_PATH="/etc/init.d/mango"
 
 echo "starting to auto publish mango system..."
 cd ~/work/mango/
@@ -21,7 +23,7 @@ ssh -T orchard@10.0.0.173 rm -rf $SERVER_HOME/download/mango/*
 scp dist/mango-1.5.zip $REMOTE_ADDR:$SERVER_HOME/download/mango/
 wait
 echo "transfer finished."
-ssh -T orchard@10.0.0.173 sudo /etc/init.d/mango15 stop
+ssh -T orchard@10.0.0.173 sudo $MANGO_PATH stop
 wait
 echo "mango stopped"
 ssh -T orchard@10.0.0.173 unzip $SERVER_HOME/download/mango/*.zip -d $SERVER_HOME/download/mango 
@@ -31,7 +33,6 @@ wait
 ssh -T orchard@10.0.0.173 cp -r $SERVER_HOME/download/mango/mango-1.5/lib $SERVER_HOME/mango-1.5
 echo "copy lib finished.."
 wait
-ssh -T orchard@10.0.0.173 sudo /etc/init.d/mango15 start
-wait
+nohup ssh -T orchard@10.0.0.173 sudo $MANGO_PATH start &
 echo "mango published successfully..."
 
