42a3f3627893affa09307ce34f4caaa63a4a82a4
XiongNeng <yidao620@gmail.com>
2015-04-07 14:29:58 +0800

    maven deploy

--- 03_deploy/maven.sh
+++ 03_deploy/maven.sh
@@ -9,8 +9,18 @@ svn update --username licf --password lcf0623
 mvn clean && mvn package
 rm -rf /home/orchard/apache-tomcat-8.0.20/webapps/ROOT/*
 unzip target/*.war -d /home/orchard/apache-tomcat-8.0.20/webapps/ROOT/
+echo "开始修改测试数据库"
+jdbc_config='/home/orchard/apache-tomcat-8.0.20/webapps/ROOT/WEB-INF/classes/jdbc.properties'
+sed -i "/^jdbc.url=/{s/\/fastloan?/\/fastloan_test?/}" $jdbc_config
+echo "先kill tomcat进程..."
+ps aux |grep tomcat |grep -v "grep tomcat" |while read line
+do
+    linewords=($line)
+    pid="${linewords[1]}"
+    sudo kill -9 $pid
+done
 
 echo "开始重启tomcat服务器..."
-/home/orchard/apache-tomcat-8.0.20/bin/catalina.sh stop
+#/home/orchard/apache-tomcat-8.0.20/bin/catalina.sh stop
 /home/orchard/apache-tomcat-8.0.20/bin/catalina.sh start > /dev/null &
 echo "自动部署成功！！！"
