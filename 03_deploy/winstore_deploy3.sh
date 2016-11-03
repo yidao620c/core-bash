725b78e77f4fb59a472612a532c41705a9362cd3
XiongNeng <yidao620@gmail.com>
2016-11-03 11:56:12 +0800

    add a winstore deploy

--- 03_deploy/winstore_deploy3.sh
+++ 03_deploy/winstore_deploy3.sh
@@ -32,7 +32,7 @@ cd /root/winstore_xn
 
 time=$(date '+%Y%m%d%H%M%S')
 
-echo "先创建空文件夹$install_dir"
+echo "先创建空文件夹 $install_dir"
 if [ -d "$install_dir" ]; then
 echo "installer dir exists..."
 rm -rf "${install_dir}/*"
@@ -43,13 +43,24 @@ fi
 echo "install_dir=$install_dir"
 cd ${install_dir}
 cp -r ../winstore-base/* .
+tar zxf winstore_core.tar.gz
+rm -f winstore_core.tar.gz
 /bin/cp -r ../winstore/* opt/sdsom/venv/lib/python2.6/site-packages/sdsom-2.1.0-py2.6.egg/sdsom/
 /bin/cp -r ../winstore-web/sdsomweb/* opt/sdsom/webapp/content/sdsomweb/
-
+#chown -R sdsadmin:sdsadmin opt/sdsom/
+#chown -R apache:apache opt/sdsom/var/log/sdsom/
+#chown -R apache:apache opt/sdsom/var/log/httpd/
 tar -zcf winstore_sdsom.tar.gz opt/sdsom/venv/lib/python2.6/site-packages/sdsom-2.1.0-py2.6.egg/sdsom/
 tar -zcf winstore_sdsomweb.tar.gz opt/sdsom/webapp/content/sdsomweb/
 rm -rf opt/sdsom/venv/lib/python2.6/site-packages/sdsom-2.1.0-py2.6.egg/sdsom/
 rm -rf opt/sdsom/webapp/content/sdsomweb/
+find opt/sdsom/etc/rc.d/init.d -type f -exec chmod +x {} \;
+find opt/sdsom/tools/ -type f -exec chmod +x {} \;
+find opt/sdsom/sbin -type f -exec chmod +x {} \;
+find opt/sdsom/usr/sbin/ -type f -exec chmod +x {} \;
+find opt/sdsom/usr/bin/ -type f -exec chmod +x {} \;
+find opt/sdsom/venv/bin/ -type f -exec chmod +x {} \;
+find opt/sdsom/uninstall -type f -exec chmod +x {} \;
 tar -zcf winstore_core.tar.gz opt/
 echo "打完新包就删除原来文件夹opt/"
 rm -rf opt/
@@ -63,34 +74,21 @@ for file_name in "${names[@]}"; do
     echo "最后开始打包安装包"
     if [[ $file_name == *"centos6"* ]]; then
         echo "this is for centos6"
-	rsync -av --exclude='winhongceph_centos7.2' --exclude='winhongtgt_centos7.2.tar.gz' --exclude='winstore3.0_centos7.2_depend_rpm.tar.gz' "${install_dir}_temp"/ ${install_dir}
-	cd ${install_dir}
-        cd winhongceph_centos6.8
-	tar -zcf winhongceph_centos6.8.tar.gz winhongceph
-        mv winhongceph_centos6.8.tar.gz ..
-        cd ..
-	rm -rf winhongceph_centos6.8
-	cd ..
+	rsync -av --exclude='winhongceph_centos7.2.tar.gz' --exclude='winhongtgt_centos7.2.tar.gz' --exclude='winstore3.0_centos7.2_depend_rpm.tar.gz' "${install_dir}_temp"/ ${install_dir}
     else
         echo "this is for centos7"
-	rsync -av --exclude='winhongceph_centos6.8' --exclude='winhongtgt_centos6.8.tar.gz' --exclude='winstore3.0_centos6.8_depend_rpm.tar.gz' "${install_dir}_temp"/ ${install_dir}
-	cd ${install_dir}
-        cd winhongceph_centos7.2
-	tar -zcf winhongceph_centos7.2.tar.gz winhongceph
-        mv winhongceph_centos7.2.tar.gz ..
-        cd ..
-	rm -rf winhongceph_centos7.2
-	cd ..
+	rsync -av --exclude='winhongceph_centos6.8.tar.gz' --exclude='winhongtgt_centos6.8.tar.gz' --exclude='winstore3.0_centos6.8_depend_rpm.tar.gz' "${install_dir}_temp"/ ${install_dir}
     fi
     new_time=$(date '+%Y%m%d')
     new_file_name="${new_time}_winhong_winstore$1${file_name:28}"
+    find ${install_dir} -name "*.sh" -type f -exec chmod +x {} \;
+    chmod +x "${install_dir}/service_sandstone"
     tar -zcf "${new_file_name}" "${install_dir}"
     rm -rf ${install_dir}
 
     echo "将升级后的包上传至samba服务器"
     /bin/cp "${new_file_name}" /mnt/winhong_ceph/winstore3.0Beta/"${new_file_name}"
     rm -f "${new_file_name}"
-
 done
 
 rm -rf "${install_dir}_temp"
