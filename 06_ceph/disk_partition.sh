703f9344f838a129fd61bf20cc9deee0fc1200be
XiongNeng <yidao620@gmail.com>
2017-03-23 16:16:00 +0800

    add a disk partions

--- 06_ceph/disk_partition.sh
+++ 06_ceph/disk_partition.sh
@@ -7,11 +7,11 @@ if [[ "$#" < 3 ]]; then
     exit 1
 fi
 
+divice_name="$1"
 disk="/dev/$1"   # 设备名如/dev/sdb
 total_num="$2"   # 新增分区数量如 2
 part_size="$3"   # 每个分区大小(GB)
-
-parted $disk print | grep "Partition Table: gpt"
+parted $disk print | grep "Partition Table: gpt" 1>/dev/null
 if [[ "$?" != "0" ]]; then
     # 转换成gpt先
     parted $disk mklabel gpt
@@ -24,21 +24,27 @@ pnum=$(sgdisk -p $disk | grep -A1 "Start (sector)" |wc -l)
 nextp=1
 # 新分区start扇区号
 start=0
-if [[ "$pnum" == "0" ]]; then
-    echo "这是一个全新的盘..."
+# 记录新创建的分区
+declare -a new_parts
+# randstr=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+# tmpfile="/tmp/${divice_name}_${randstr}"
+if [[ "$pnum" -le "1" ]]; then
+    # echo "这是一个全新的盘..."
     start=$(sgdisk -p $disk | grep "First usable sector is" | awk -F, '{print $1}' | awk '{print $NF}')
 else
-    echo "这个盘已经有分区了，在后面继续分区..."
+    # echo "这个盘已经有分区了，在后面继续分区..."
     for e in $(sgdisk -p $disk | sed -n '/Start (sector)/,$p' | sed -n '2,$p' | awk '{print $3}')
     do
-        if [[ "$start" < "$e" ]]; then
+        if [[ "$start" -lt "$e" ]]; then
             start="$e"
         fi
     done
 
+    (( start++ ))
+
     for e in $(sgdisk -p $disk | sed -n '/Start (sector)/,$p' | sed -n '2,$p' | awk '{print $1}')
     do
-        if [[ "$nextp" < "$e" ]]; then
+        if [[ "$nextp" -lt "$e" ]]; then
             nextp="$e"
         fi
     done
@@ -49,12 +55,17 @@ fi
 (( end = start + sector_num - 1 ))
 
 # 开始连续分区
-for (( i = 1; i <= total_num; i++ )); do
-    echo "开始划分第${i}个分区"
-    sgdisk -n "$nextp":"$start":"$end" -t "$nextp":8300 -c "${nextp}":"ceph journal${nextp}" -p $disk 1>/dev/null
+for (( i = 0; i < total_num; i++ )); do
+    # echo "开始划分第$((i+1))个分区"
+    j_name="ceph journal ${divice_name} ${nextp}"
+    sgdisk -n ${nextp}:${start}:${end} -t ${nextp}:45b0969e-9b03-4f30-b4c6-b4b80ceff106 -p $disk 1>/dev/null
+    sgdisk -c ${nextp}:"${j_name}" $disk 1>/dev/null
+    # 分区名写入tmpfile
+    each_partname=$(blkid | grep "PARTLABEL=\"${j_name}\"" | awk '{print $1}' | awk '{print substr($0, 1, length($0)-1)}')
+    new_parts["$i"]="${each_partname}"
     (( nextp++ ))
     (( start = end + 1 ))
     (( end = start + sector_num - 1 ))
 done
-echo "分区划分完成"
+echo "${new_parts[@]}"
 
