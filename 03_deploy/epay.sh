4091524fe188a861623047afb08116c3d380fa1e
yidao620 <yidao620@gmail.com>
2017-11-24 11:21:03 +0800

    增加一个自动发布springboot工程的脚本

--- 03_deploy/epay.sh
+++ 03_deploy/epay.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-# 云彩付自动更新发布脚本
+# 云彩付自动更新脚本
 # 先clone相应的分支下来：
 # git clone -b data ssh://git@120.24.173.142:7999/epay/clouds-epay-web.git clouds-epay-web-data
 # 各个运行环境的配置yml文件分开了
+# 远程调试启动：
+# nohup java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar -Dspring.profiles.active=${profile} ${jarfile} >/dev/null 2>&1 &
 
 function start {
     profile="$1"
-    jarfile=$(ls target/*.jar)
     echo "启动环境profile=${profile}"
-    stop $profile $jarfile
+    jarfile=$(ls target/*.jar)
+    if [[ "$?" == "0" ]]; then
+        stop $profile $jarfile
+    fi
     branch=$(git branch |awk '{print $2}')
     git pull origin ${branch}
     echo "更新完代码开始重新打包"
@@ -42,17 +46,29 @@ if [[ "$1" == "start" ]]; then
         echo "请输入正确参数：./epay.sh start {profile}"
         exit 1
     fi
-    start $2
+    profile="$2"
+    if [[ "$profile" != "dev" || "$profile" != "test" || "$profile" != "show" || "$profile" != "production" ]]; then
+        echo "参数错误，请输入正确的profile参数，使用方法：
+        echo "./epay.sh start {profile}    # 启动应用，{profile}运行环境，目前只能输入四个值：dev/test/show/production"
+        exit 1
+    fi
+    start "${profile}"
 elif [[ "$1" == "stop" ]]; then
     if [[ "$#" < 2 ]]; then
         echo "请输入正确参数：./epay.sh stop  {profile}"
         exit 1
     fi
+    profile="$2"
+    if [[ "$profile" != "dev" || "$profile" != "test" || "$profile" != "show" || "$profile" != "production" ]]; then
+        echo "参数错误，请输入正确的profile参数，使用方法：
+        echo "./epay.sh stop {profile}     # 停止应用，{profile}运行环境，目前只能输入四个值：dev/test/show/production"
+        exit 1
+    fi
     jarfile=$(ls target/*.jar)
-    stop $2 $jarfile
+    stop $profile $jarfile
 else
     echo "参数错误，使用方法：{}参数是必填的，[]参数可选"
-    echo "./epay.sh start {profile}    # 启动应用，{profile}运行环境"
-    echo "./epay.sh stop  {profile}    # 停止应用，{profile}运行环境"
+    echo "./epay.sh start {profile}    # 启动应用，{profile}运行环境，目前只能输入四个值：dev/test/show/production"
+    echo "./epay.sh stop  {profile}    # 停止应用，{profile}运行环境，目前只能输入四个值：dev/test/show/production"
     exit 1
 fi
